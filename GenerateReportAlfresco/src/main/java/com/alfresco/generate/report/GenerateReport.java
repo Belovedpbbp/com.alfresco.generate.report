ee4957f63516087ea207370015fd125f042b4977
PingPing <piyanatc2612@gmail.com>
2014-03-28 10:10:46 +0700

    แก้ไข code ให้ดึง  template ออกมา

--- GenerateReportAlfresco/src/main/java/com/alfresco/generate/report/GenerateReport.java
+++ GenerateReportAlfresco/src/main/java/com/alfresco/generate/report/GenerateReport.java
@@ -20,29 +20,23 @@ public class GenerateReport {
 	public String generateReport(Map<String, Object> param) throws JRException,
 			IOException {
 		GenerateDatasource generateDatasource = new GenerateDatasource();
-		
-		InputStream reportFile = ClassLoader.class
-				.getResourceAsStream("/AlfrescoReport.jrxml");
 
-		JasperReport compileReport = JasperCompileManager.compileReport(reportFile);
-		
+		InputStream reportFile = getClass().getResourceAsStream(
+				"/AlfrescoReport.jrxml");
+
+		JasperReport compileReport = JasperCompileManager
+				.compileReport(reportFile);
+
 		JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport,
 				param, generateDatasource.datasource());
 
-		String destFileName = "C:/Alfresco/tomcat/shared/classes/alfresco/extension/DataTemplate/AlfrescoReport.pdf";
+		String outPutFile = "C:/Alfresco/tomcat/shared/classes/alfresco/extension/DataTemplate/AlfrescoReport.pdf";
 
 		String returnText = null;
-		
-		if (jasperPrint == null) {
-			System.out
-					.println("Cannot Export Report Because No File JasperReport");
-		} else {
 
-			JasperExportManager
-					.exportReportToPdfFile(jasperPrint, destFileName);
-			
-			returnText = "Complete GenerateReport";
-		}
+		JasperExportManager.exportReportToPdfFile(jasperPrint, outPutFile);
+
+		returnText = "Complete GenerateReport";
 		return returnText;
 	}
 
