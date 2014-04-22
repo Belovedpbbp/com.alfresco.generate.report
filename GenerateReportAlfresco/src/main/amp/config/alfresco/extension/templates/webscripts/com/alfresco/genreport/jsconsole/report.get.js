9f664784615d308865ab8db627a89e677498a28c
pingping <piyanatc2612@gmail.com>
2014-04-22 15:12:31 +0700

    Revert "Code upload File ที่ชื่อซ้ำกัน บน javascript console "
    This reverts commit f90c592e98f1b05e51d6758778afb6a562b89e71.

--- GenerateReportAlfresco/src/main/amp/config/alfresco/extension/templates/webscripts/com/alfresco/genreport/jsconsole/report.get.js
+++ GenerateReportAlfresco/src/main/amp/config/alfresco/extension/templates/webscripts/com/alfresco/genreport/jsconsole/report.get.js
@@ -21,31 +21,18 @@ var objCount = {
 var reportsFolderName = "AlfrescoJasperReports";
 
 var reports = companyhome.childByNamePath(reportsFolderName);
-if (!reports) {
+if (!reports)
 	reports = companyhome.createFolder(reportsFolderName);
-}
 
 var node = reports.nodeRef;
 
-var calendar = new Packages.java.util.Calendar.getInstance();
-
-var simpleDateFormat = new Packages.java.text.SimpleDateFormat("yyyy-MM-dd");
-
-var currentDate = simpleDateFormat.format(calendar.getTime());
-
-var reportName = "AlfrescoReport " + currentDate + ".pdf";
-
-var i = 0;
-while (generateReport.reportExists(node, reportName)) {
-	reportName = "AfrescoReport " + currentDate + "(" + (++i) + ")" + ".pdf";
-}
-
-convertJsToJava.objectToMap(objCount, node, reportName);
-
 try {
+	var scriptConvert = convertJsToJava.objectToMap(objCount, node, "pdf");
+
 	model.genReport = "AlfrescoJasperReports - report generated. to "
 			+ reports.displayPath + "/" + reports.name;
 } catch (err) {
 	model.genReport = "AlfrescoJasperReports - error while generating report... :( "
 			+ err;
 }
+
